40694f87c92fd88ef6869e0aa39da1d6084dafc1
Astro <rgrxmahabub.official@gmail.com>
2023-05-19 18:40:42 +0600

    update api added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -27,9 +27,73 @@ async function run() {
 
     const toysCollection = client.db("playfulDelights").collection("allToys");
 
+    const indexKeys = { toyName: 1, toyCategory: 1 };
+    const indexOptions = { name: "nameCategory" };
+
+    const result = await toysCollection.createIndex(indexKeys, indexOptions);
+
     app.get("/allToys", async (req, res) => {
       const cursor = toysCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/getToysByText/:text", async (req, res) => {
+      const text = req.params.text;
+      const result = await toysCollection
+        .find({
+          $or: [
+            { toyName: { $regex: text, $options: "i" } },
+            { toyCategory: { $regex: text, $options: "i" } },
+          ],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/myToys/:email", async (req, res) => {
+      const result = await toysCollection
+        .find({ sellerEmail: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/addToy", async (req, res) => {
+      const body = req.body;
+      const result = await toysCollection.insertOne(body);
+      res.send(result);
+    });
+
+    app.put("/updatedToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          toyName: body.toyName,
+          toyPictureUrl: body.toyPictureUrl,
+          rating: body.rating,
+          price: body.price,
+          description: body.description,
+          availableQuantity: body.availableQuantity,
+        },
+      };
+      const result = await toysCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    
+    app.delete("/remove/:id", async (req, res) => {
+      const result = await toysCollection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
       res.send(result);
     });
 
