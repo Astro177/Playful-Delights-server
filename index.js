0bc1e77a7db6d48774646b333295fbb5339ba06d
Astro <rgrxmahabub.official@gmail.com>
2023-05-19 23:23:26 +0600

    single data api fixed

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/allToys/:id", async (req, res) => {
+    app.get("/toys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toysCollection.findOne(query);
